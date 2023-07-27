7e1c11bebb6693647874a5ca0020ad773aef830f
gcc666666 <237692407@qq.com>
2023-07-27 18:20:46 +0800

    2023072703 gcc 修改rename_save文件API接口，文件读取路径

--- test_dir/gcc/test_renama_save.py
+++ test_dir/gcc/test_renama_save.py
@@ -17,8 +17,8 @@ class Test_Export_tgz():
     @pytest.mark.parametrize("job_id", GetTestData().get_job_id('Rename_save'))
     def test_output_gerber274x(self, job_id):
         '''
-        本用例测试 Rename、Save功能。
-        导入tgz删除step、删除layer，更改step、job名之后save导出
+        本用例测试 Rename、Save功能。料号ID：21648
+        导入tgz删除step、删除layer，更改step、job名之后save
         '''
         print('分割线'.center(192, "-"))
 
@@ -30,25 +30,18 @@ class Test_Export_tgz():
         # 取到临时目录
         temp_path = RunConfig.temp_path_base + "_" + str(job_id) + "_" + vs_time_g
         temp_compressed_path = os.path.join(temp_path, 'compressed')
-        temp_save_path = os.path.join(temp_path, 'save')
-        temp_export_tgz_path = os.path.join(temp_path, 'export_tgz')
 
         # --------------------------------下载测试资料--tgz文件，并解压完，文件夹名称作为料号名称-------------------------------
         job = DMS().get_file_from_dms_db(temp_path, job_id, field='file_compressed', decompress='tgz')
-
-        os.mkdir(temp_save_path)
-        os.mkdir(temp_export_tgz_path)
-        # print(temp_export_tgz_path)
 
         # 用悦谱CAM打开料号
         Input.open_job(job, temp_compressed_path)  # 用悦谱CAM打开料号
         all_layers_list_job = Information.get_layers(job)
         all_step_list_job = Information.get_steps(job)
 
+        # 删除以及保留的layer、step
         delete_layers = [x for x in all_layers_list_job if len(str(x)) > 3]
         delete_step = all_step_list_job[1:]
-        layers = [x for x in all_layers_list_job if x not in delete_layers]
-        steps = [x for x in all_step_list_job if x not in delete_step]
 
         # 删除step、layer
         for layer in delete_layers:
@@ -56,25 +49,21 @@ class Test_Export_tgz():
         for step in delete_step:
             Matrix.delete_step('2625262a', step)
 
+        # 更名、保存
         Job.rename_job('2625262a', '2625262a-orig')
-        Output.save_job('2625262a-orig', temp_save_path)
-        Application.export_job_jwApp('2625262a-orig', temp_export_tgz_path, ['tgz'])
-
-        tar = tarfile.open(os.path.join(temp_export_tgz_path, '2625262a-orig.tgz'), 'r:gz')
-        files_name = tar.getnames()
-        for file_name in files_name:
-            tar.extract(file_name, temp_export_tgz_path)
-        tar.close()
-
+        BASE.save_job('2625262a-orig')
         # GUI.show_matrix('2625262a-orig')
         # GUI.show_layer('8037049a', 'net', 'l1')
-        steps_list = os.listdir(os.path.join(temp_export_tgz_path, '2625262a-orig\\steps'))
-        layers_list = os.listdir(os.path.join(temp_export_tgz_path, '2625262a-orig\\steps\\orig\\layers'))
 
-        assert steps_list == steps
-        assert sorted(layers_list) == sorted(layers)
+        # 获取save之后的文件层别信息
+        steps_list = os.listdir(os.path.join(temp_compressed_path, '2625262a-orig\\steps'))
+        layers_list = os.listdir(os.path.join(temp_compressed_path, '2625262a-orig\\steps\\orig\\layers'))
 
-        # if steps_list == steps:
+        # 获取EP—CAM层别信息
+        steps = Information.get_layers('2625262a-orig')
+        layers = Information.get_steps('2625262a-orig')
+
+        # if sorted(steps_list) == sorted(steps):
         #     print('测试通过')
         # else:
         #     print('测试失败')
@@ -87,3 +76,6 @@ class Test_Export_tgz():
         #     print('测试失败')
         #     print(sorted(layers_list))
         #     print(sorted(layers))
+
+        assert sorted(steps_list) == sorted(steps)
+        assert sorted(layers_list) == sorted(layers)
