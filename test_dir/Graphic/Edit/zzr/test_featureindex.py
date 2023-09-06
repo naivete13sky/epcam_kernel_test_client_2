b2b2a55b76231fda1c9471c919ebfccbc741cb26
naivete13sky <chen320821@163.com>
2023-09-06 17:49:40 +0800

    20230906 8 test_featureindex.py ok

--- test_dir/Graphic/Edit/zzr/test_featureindex.py
+++ test_dir/Graphic/Edit/zzr/test_featureindex.py
@@ -21,14 +21,31 @@ class TestGraphicEditFeatureIndex:
 
         g = RunConfig.driver_g  # 拿到G软件
         data = {}  # 存放比对结果信息
-        vs_time_g = str(int(time.time()))  # 比对时间
-        data["vs_time_g"] = vs_time_g  # 比对时间存入字典
-        data["job_id"] = job_id
+
         step = 'prepare'  # 定义需要执行比对的step名
         layers = ['l1','l2']  # 定义需要比对的层
         # layers = ['drl1-10']
-        # 取到临时目录
-        temp_path = RunConfig.temp_path_base + "_" + str(job_id) + "_" + vs_time_g
+        # 取到临时目录，如果存在旧目录，则删除
+        temp_path = RunConfig.temp_path_base
+        if os.path.exists(temp_path):
+            if RunConfig.gSetupType == 'local':
+                # os.remove(self.tempGerberPath)#此方法容易因权限问题报错
+                shutil.rmtree(temp_path)
+            if RunConfig.gSetupType == 'vmware':
+                # 使用PsExec通过命令删除远程机器的文件
+                from cc.cc_method import RemoteCMD
+                myRemoteCMD = RemoteCMD(psexec_path='cc', computer='192.168.1.3',
+                                        username='administrator',
+                                        password='cc')
+                command_operator = 'rd /s /q'
+                command_folder_path = os.path.join(RunConfig.temp_path_g)
+                command = r'cmd /c {} "{}"'.format(command_operator, command_folder_path)
+                myRemoteCMD.run_cmd(command)
+                print("remote delete finish")
+
+
+
+
         temp_compressed_path = os.path.join(temp_path, 'compressed')
         temp_ep_path = os.path.join(temp_path, 'ep')
         temp_g_path = os.path.join(temp_path, 'g')
@@ -52,28 +69,56 @@ class TestGraphicEditFeatureIndex:
         Layers.hierarchy_edit(job_case, step, ['l2'], 0)
 
         save_job(job_case, temp_ep_path)
-        GUI.show_layer(job_case,step,'l1')
+        # GUI.show_layer(job_case,step,'l1')
+
+
+
 
         # ----------------------------------------开始比图：G与EP---------------------------------------------------------
         print('比图--G转图VS悦谱转图'.center(190, '-'))
-        job_yg_remote_path = r'\\vmware-host\Shared Folders\share/{}/g/{}'.format(
-            'temp' + "_" + str(job_id) + "_" + vs_time_g, job_yg)
+        job_yg_remote_path = os.path.join(RunConfig.temp_path_g,'g',job_yg)
         print("job_yg_remote_path:", job_yg_remote_path)
-        job_case_remote_path = r'\\vmware-host\Shared Folders\share/{}/ep/{}'.format(
-            'temp' + "_" + str(job_id) + "_" + vs_time_g, job_case)
+        job_case_remote_path = os.path.join(RunConfig.temp_path_g, 'ep', job_case)
         print("job_testcase_remote_path:", job_case_remote_path)
         # # 导入要比图的资料
         g.import_odb_folder(job_yg_remote_path)
         g.import_odb_folder(job_case_remote_path)
 
-        r = g.layer_compare_dms(job_id=job_id, vs_time_g=vs_time_g, temp_path=temp_path,
-                                job1=job_yg, step1=step, all_layers_list_job1=layers, job2=job_case, step2=step,
-                                all_layers_list_job2=layers, adjust_position=True)
-        data["all_result_g1"] = r['all_result_g']
-        data["all_result"] = r['all_result']
-        data['g1_vs_total_result_flag'] = r['g_vs_total_result_flag']
-        Print.print_with_delimiter("断言--看一下G1转图中的层是不是都有比对结果")
-        assert len(layers) == len(r['all_result_g'])
+        layerInfo = []
+        # Input.open_job(job_yg, temp_g_path)  # 用悦谱CAM打开料号
+        # all_layers_list_job_g = Information.get_layers(job_yg)
+        for each in ['l1','l2']:
+            each_dict = {}
+            each_dict["layer"] = each.lower()
+            each_dict['layer_type'] = ''
+            layerInfo.append(each_dict)
+
+        print("layerInfo:", layerInfo)
+        job1, job2 = job_yg, job_case
+        step1, step2 = 'prepare', 'prepare'
+        g.layer_compare_g_open_2_job(job1=job1, step1=step1, job2=job2, step2=step2)
+
+        # 校正孔用
+        temp_path_local_info1 = os.path.join(temp_path, 'info1')
+        if not os.path.exists(temp_path_local_info1):
+            os.mkdir(temp_path_local_info1)
+        temp_path_local_info2 = os.path.join(temp_path, 'info2')
+        if not os.path.exists(temp_path_local_info2):
+            os.mkdir(temp_path_local_info2)
+
+        compareResult = g.layer_compare(temp_path=temp_path, temp_path_g=RunConfig.temp_path_g,
+                                        job1=job1, step1=step1,
+                                        job2=job2, step2=step2,
+                                        layerInfo=layerInfo,
+                                        adjust_position=True, jsonPath=r'my_config.json')
+        print('compareResult_input_vs:', compareResult)
+        data["all_result_g1"] = compareResult['all_result_g']
+        data['g1_vs_total_result_flag'] = compareResult['g_vs_total_result_flag']
+
+
+
+
+
 
         # ----------------------------------------开始验证结果--------------------------------------------------------
         Print.print_with_delimiter('比对结果信息展示--开始')
