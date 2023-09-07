a67c08f3a4d66ce2b1f5e0de4154d98e13d6ad24
naivete13sky <chen320821@163.com>
2023-09-07 14:20:26 +0800

    20230907 5

--- test_dir/Engineering/test_input_output.py
+++ test_dir/Engineering/test_input_output.py
@@ -2,11 +2,8 @@ import pytest, os, time, json, shutil, sys
 from config import RunConfig
 from cc.cc_method import GetTestData, DMS, Print, getFlist, CompressTool
 from config_ep.epcam_cc_method import MyInput, MyOutput
-from config_g.g_cc_method import GInput
 from epkernel import Input, GUI, BASE
 from epkernel.Action import Information
-from epkernel.Edition import Layers
-from datetime import datetime
 
 
 @pytest.mark.input_output
@@ -19,7 +16,6 @@ class TestInputOutputBasicGerber274X:
 
         g = RunConfig.driver_g#拿到G软件
         data = {}#存放比对结果信息
-
         # 取到临时目录，如果存在旧目录，则删除
         temp_path = RunConfig.temp_path_base
         if os.path.exists(temp_path):
@@ -47,14 +43,11 @@ class TestInputOutputBasicGerber274X:
 
         # ----------悦谱转图。先下载并解压原始gerber文件,拿到解压后的文件夹名称，此名称加上_ep就是我们要的名称。然后转图。-------------
         job_ep = DMS().get_file_from_dms_db(temp_path, job_id, field='file_compressed', decompress='rar')
-        # print('job_ep:',job_ep)
         MyInput(folder_path = os.path.join(temp_gerber_path, os.listdir(temp_gerber_path)[0].lower()),
                 job = job_ep, step = r'orig', job_id = job_id, save_path = temp_ep_path)
         all_layers_list_job_ep = Information.get_layers(job_ep)
-        # print('all_layers_list_job_ep:',all_layers_list_job_ep)
-
         # GUI.show_layer(job_ep, 'orig', all_layers_list_job_ep[0])
-        # time.sleep(1000)
+
 
         # --------------------------------下载G转图tgz，并解压好，获取到文件夹名称，作为g料号名称-------------------------------
         job_g = DMS().get_file_from_dms_db(temp_path, job_id, field='file_odb_g', decompress='tgz')
@@ -72,7 +65,6 @@ class TestInputOutputBasicGerber274X:
             each_dict["layer"] = each.lower()
             each_dict['layer_type'] = 'drill' if each in drill_layers else ''
             layerInfo.append(each_dict)
-
         print("layerInfo:",layerInfo)
 
         job1, job2 = job_g, job_ep
@@ -84,8 +76,6 @@ class TestInputOutputBasicGerber274X:
         g.import_odb_folder(job_ep_remote_path)
         g.layer_compare_g_open_2_job(job1=job1, step1=step1, job2=job2, step2=step2)
 
-
-
         # 校正孔用
         temp_path_local_info1 = os.path.join(temp_path, 'info1')
         if not os.path.exists(temp_path_local_info1):
@@ -94,17 +84,14 @@ class TestInputOutputBasicGerber274X:
         if not os.path.exists(temp_path_local_info2):
             os.mkdir(temp_path_local_info2)
 
-
         compareResult = g.layer_compare(temp_path=temp_path, temp_path_g=RunConfig.temp_path_g,
                                              job1=job1, step1=step1,
                                              job2=job2, step2=step2,
                                              layerInfo=layerInfo,
                                              adjust_position=True, jsonPath=r'my_config.json')
-        print('compareResult_input_vs:',compareResult)
         data["all_result_g"] = compareResult['all_result_g']
         data['g_vs_total_result_flag'] = compareResult['g_vs_total_result_flag']
         assert len(all_layers_list_job_g) == len(compareResult['all_result_g'])
-
 
 
         # ----------------------------------------开始测试输出gerber功能---------------------------------------------------
@@ -118,46 +105,25 @@ class TestInputOutputBasicGerber274X:
         step = 'orig'
         file_path = os.path.join(temp_path, ep_out_put_gerber_folder)
         gerberList = getFlist(file_path)
-        print(gerberList)
-        g_temp_path = RunConfig.temp_path_g
-        # gerberList_path = []
-        # for each in gerberList:
-        #     gerberList_path.append(os.path.join(g_temp_path, r'output_gerber', job_ep, r'orig', each))
-        # print(gerberList_path)
-
         temp_out_put_gerber_g_input_path = os.path.join(temp_path, 'g2')
         if os.path.exists(temp_out_put_gerber_g_input_path):
             shutil.rmtree(temp_out_put_gerber_g_input_path)
         os.mkdir(temp_out_put_gerber_g_input_path)
         out_path = temp_out_put_gerber_g_input_path
 
-        # g.gerber_to_odb_batch(job_g2, step, gerberList_path, out_path, job_id, drill_para='epcam_default')
         gerberList_path = []
-
         for each in gerberList:
             each_dict = {}
-            each_dict['path'] = os.path.join(g_temp_path, r'output_gerber', job_ep, r'orig', each)
+            each_dict['path'] = os.path.join(RunConfig.temp_path_g, r'output_gerber', job_ep, r'orig', each)
             layer_e2 = DMS().get_job_layer_fields_from_dms_db_pandas_one_layer(job_id,filter=each.replace(' ', '-').replace('(', '-').replace(')', '-'))
             if layer_e2.status.values[0] == 'published' and layer_e2.layer_file_type.values[0]=='excellon2':
                 each_dict['file_type'] = 'excellon'
                 each_dict_para = {}
-                print('layer_e2cc:',layer_e2)
-                # each_dict_para['zeroes'] = layer_e2.zeroes_omitted.values[0].lower()
-                # each_dict_para['nf1'] = int(layer_e2.number_format_A.values[0])
-                # each_dict_para['nf2'] = int(layer_e2.number_format_B.values[0])
-                # each_dict_para['units'] = layer_e2.units.values[0].lower()
-                # # g软件的tool_units没有mils选项
-                # if layer_e2.tool_units_g.values[0].lower() == 'mils':
-                #     each_dict_para['tool_units'] = 'inch'
-                # else:
-                #     each_dict_para['tool_units'] = layer_e2.tool_units_g.values[0].lower()
-
                 each_dict_para['units'] = 'inch'
                 each_dict_para['zeroes'] = 'none'
                 each_dict_para['nf1'] = "2"
                 each_dict_para['nf2'] = "6"
                 each_dict_para['tool_units'] = 'mm'
-
                 each_dict['para'] = each_dict_para
                 gerberList_path.append(each_dict)
             else:#不是孔就当作是gerber处理
@@ -165,7 +131,7 @@ class TestInputOutputBasicGerber274X:
                 gerberList_path.append(each_dict)
         g.input_init(job=job_g2, step=step, gerberList_path=gerberList_path,jsonPath=r'D:\cc\python\epwork\epcam_kernel_test_client_2\my_config.json')
         # 输出tgz到指定目录
-        g.g_export(job_g2, os.path.join(g_temp_path, r'g2'))
+        g.g_export(job_g2, os.path.join(RunConfig.temp_path_g, r'g2'))
 
         # ----------------------------------------开始用G软件比图，g1和g2--------------------------------------------------------
         # 再导入一个标准G转图，加个后缀1。
@@ -175,12 +141,6 @@ class TestInputOutputBasicGerber274X:
         temp_g1_compare_result_path = os.path.join(temp_path, g1_compare_result_folder)
         if not os.path.exists(temp_g1_compare_result_path):
             os.mkdir(temp_g1_compare_result_path)
-
-
-
-
-
-
 
         #校正孔用
         temp_path_local_info1 = os.path.join(temp_path, 'info1')
@@ -206,7 +166,6 @@ class TestInputOutputBasicGerber274X:
         assert len(all_layers_list_job_g) == len(compareResult['all_result_g'])
 
         # 获取原始层文件信息，最全的
-
         all_result = {}  # all_result存放原始文件中所有层的比对信息
         for layer_org in all_layer_from_org:
             layer_org_find_flag = False
@@ -219,44 +178,6 @@ class TestInputOutputBasicGerber274X:
                 all_result[layer_org] = layer_org_vs_value
             else:
                 all_result[layer_org] = 'G转图中无此层'
-
-
-
-
-        # ----------------------------------------开始用EP软件比图，g和g2--------------------------------------------------
-        # all_result_ep_vs_g_g2 = {}
-        # # 打开job_g
-        # # 前面打开过的。直接show下看看
-        # # GUI.show_layer(job_g, 'orig', 'top')
-        #
-        # # 打开 job_g2
-        # #先解压tgz
-        # CompressTool.untgz(os.path.join(temp_g2_path, os.listdir(temp_g2_path)[0]),temp_g2_path)
-        # Input.open_job(job_g2, temp_g2_path)
-        # # GUI.show_layer(job_g2, 'orig', 'top')
-        #
-        # # 开始用kernel比对
-        # for each_layer_g in all_layers_list_job_g:
-        #     print('EP VS'.center(192,'-'))
-        #     print('层名称：',each_layer_g)
-        #     ep_layer_compare_result = BASE.layer_compare_point(job_g, 'orig', each_layer_g, job_g2, 'orig', each_layer_g, 22860,True, True, 5080000)
-        #     ep_layer_compare_result = json.loads(ep_layer_compare_result)
-        #     print(ep_layer_compare_result)
-        #     print(len(ep_layer_compare_result['result']))
-        #     if len(ep_layer_compare_result['result']) > 0:
-        #         all_result_ep_vs_g_g2[each_layer_g] = '错误'
-        #         print('错误！')
-        #         # Layers.layer_compare(job_g, 'orig', each_layer_g, job_g2, 'orig', each_layer_g, 22860,True, True, each_layer_g + '-com', 5080000)
-        #         # GUI.show_layer(job_g, 'orig', each_layer_g)
-        #     if len(ep_layer_compare_result['result']) == 0:
-        #         all_result_ep_vs_g_g2[each_layer_g] = '正常'
-        #         print('正常！')
-        #
-        # assert len(all_result_ep_vs_g_g2) == len(all_layers_list_job_g)
-
-
-
-
 
         # ----------------------------------------开始验证结果--------------------------------------------------------
 
@@ -272,8 +193,6 @@ class TestInputOutputBasicGerber274X:
         print('分割线'.center(198,'-'))
         print('G1转图的层：', data["all_result_g1"])
         print('分割线'.center(198,'-'))
-        # print('悦谱比图结果：', all_result_ep_vs_g_g2)
-        # Print.print_with_delimiter('比对结果信息展示--结束')
 
         Print.print_with_delimiter("断言--开始")
         assert data['g_vs_total_result_flag'] == True
@@ -284,10 +203,7 @@ class TestInputOutputBasicGerber274X:
         for key in data['all_result_g1']:
             assert data['all_result_g1'][key] == "正常"
 
-        # for key in all_result_ep_vs_g_g2:
-        #     assert all_result_ep_vs_g_g2[key] == "正常"
-
-        Print.print_with_delimiter("断言--结束")
+        print("断言--结束".center(192,'*'))
 
 
 
